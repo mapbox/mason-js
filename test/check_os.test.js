0686a3fe07bd11a7bab5424b49130420c393ab43
Ann Millspaugh <ann.millspaugh@gmail.com>
2017-11-14 17:33:38 -0800

    update imports and tests

--- test/check_os.test.js
+++ test/check_os.test.js
@@ -1,8 +1,8 @@
 var test = require('tape');
-var os = require('../lib/check_os.js');
+var retrievePlatformDirectoryPromise = require('../lib/check_os.js');
 
 test('check OS system and return correct platform directory', function(assert) {
-  os.retrievePlatformDirectory(function(err, result) {
+  retrievePlatformDirectoryPromise.then(function(result) {
     assert.equal(result, 'bucketone/osx-86_x64');
     assert.end();
   });
--- test/ini_file_reader.test.js
+++ test/ini_file_reader.test.js
@@ -1,5 +1,8 @@
 var test = require('tape');
 var fileReaderPromise = require('../lib/ini_file_reader.js');
+const fs = require('fs');
+const dotenvConfig = require('../config.js');
+dotenvConfig.envConfig()
 
 test('read ini file', function(assert) {
   fileReaderPromise.then(function(result) {
@@ -8,3 +11,18 @@ test('read ini file', function(assert) {
     assert.end()
   });
 });
+
+// test('ini file does not exist', function(assert) {
+//   let originalPath = process.env.MASON_INI_PATH;
+//   let testPath = './test/mason-test-versions.ini';
+//   console.log(originalPath); 
+//   fs.unlinkSync(originalPath); 
+//   fileReaderPromise.then(function(result) {
+//     console.log(result);
+//     assert.end()
+//   });
+//   fs.createReadStream(testPath).pipe(fs.createWriteStream(originalPath));
+// });
+
+
+
