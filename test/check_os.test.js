4e1040fd030b27ea174fa1fea4de647ef8eafa3c
Ann Millspaugh <ann.millspaugh@gmail.com>
2017-11-20 12:35:53 -0800

    update package retrieval to handle packages already downloaded

--- test/check_os.test.js
+++
@@ -1,9 +0,0 @@
-var test = require('tape');
-var retrievePlatformDirectoryPromise = require('../lib/check_os.js');
-
-test('check OS system and return correct platform directory', function(assert) {
-  retrievePlatformDirectoryPromise.then(function(result) {
-    assert.equal(result, 'bucketone/osx-86_x64');
-    assert.end();
-  });
-});
--- test/ini_file_reader.test.js
+++ test/ini_file_reader.test.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 const dotenvConfig = require('../config.js');
 dotenvConfig.envConfig();
 
+
 test('read ini file', function(assert) {
-  var headerPackage = {'name':'protozero', 'version':'1.5.1', 'headers':true}
-  var compiledPackage = { 'name': 'ccache', 'version': '3.6.4', 'headers': false }
+  var headerPackage = {'name':'protozero', 'version':'1.5.1', 'headers':true, 'os': null, 'awsPath': 'headers/protozero/1.5.1.tar.gz'}
+  var compiledPackage = { 'name': 'ccache', 'version': '3.6.4', 'headers': null, 'os': 'osx-x86_64', 'awsPath': 'osx-x86_64/ccache/3.6.4.tar.gz' }
+
   fileReaderPromise.then(function(result) {
     assert.deepEqual(result[0],headerPackage);
     assert.deepEqual(result[2], compiledPackage);
