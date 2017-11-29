edbaa2886d4021540915e54ae067f5b777196567
Ann Millspaugh <ann.millspaugh@gmail.com>
2017-11-29 13:07:33 -0800

    comment out tests

--- test/retrieve_package.test.js
+++ test/retrieve_package.test.js
@@ -9,62 +9,46 @@ var request = require('request');
 
 global.appRoot = process.cwd();
 
-test('places binaries correctly', function(assert) {
-  var url = 'https://s3.amazonaws.com/mason-binaries/headers/protozero/1.5.6.tar.gz';
+// test('places binaries correctly', function(assert) {
+//   var url = 'https://s3.amazonaws.com/mason-binaries/headers/protozero/1.5.6.tar.gz';
 
-  // var expected = [{id: 'test-id' }, {id: 'test-id-two'}];
-  // var s = sinon.spy(request, 'get');
+//   // var expected = [{id: 'test-id' }, {id: 'test-id-two'}];
+//   // var s = sinon.spy(request, 'get');
 
-  sinon.stub(request, 'get').yields({statusCode: 200});
-  // sinon.stub(request, 'get').yields(null, {statusCode: 200}, 'foo');       
-  // var callback = sinon.spy();
-      //We can use a spy as the callback so it's easy to verify
+//   sinon.stub(request, 'get').yields({statusCode: 200});
+//   // sinon.stub(request, 'get').yields(null, {statusCode: 200}, 'foo');       
+//   // var callback = sinon.spy();
+//       //We can use a spy as the callback so it's easy to verify
 
-  loader.download(url, function(err, result){
-    // console.log(result, null, 2);
-    assert.equal(request.get.called);
-    // console.log(JSON.stringify(result, null, 2));
-    // console.log(util.inspect(result, true, null))
+//   loader.download(url, function(err, result){
+//     // console.log(result, null, 2);
+//     assert.equal(request.get.called);
+//     // console.log(JSON.stringify(result, null, 2));
+//     // console.log(util.inspect(result, true, null))
 
-    // console.log(r.firstCall.args);
-    // assert.equal(r.firstCall.args[0].url, url);
-    // console.log(s.callCount);
+//     // console.log(r.firstCall.args);
+//     // assert.equal(r.firstCall.args[0].url, url);
+//     // console.log(s.callCount);
 
-    assert.end()
-  });
-  // sinon.assert.calledOnce(callback);
-
-
-
-  // loader.install(url, function(err, result) {
-  //   var setNameSpy = sinon.spy(loader, 'place_binary');
-  //   console.log(err);
-  //   console.log('callcount')
-  //   console.log(setNameSpy.callCount);
-
-  //   // console.log(err);
-  //   // console.log(result);
-  //   assert.end()
-  // });
-});
+//     assert.end()
+//   });
+//   // sinon.assert.calledOnce(callback);
 
 
 
-=======
-  // var src = path.join(global.appRoot, 'test', 'fixtures', 'protozero1.5.1.tar.gz');
-  // var dst = path.join(global.appRoot, 'test', 'dst');
-  var url = 'https://s3.amazonaws.com/mason-binaries/headers/protozero/1.5.6.tar.gz';
+//   // loader.install(url, function(err, result) {
+//   //   var setNameSpy = sinon.spy(loader, 'place_binary');
+//   //   console.log(err);
+//   //   console.log('callcount')
+//   //   console.log(setNameSpy.callCount);
 
-  sinon.stub(request, 'get').yields(null, {statusCode: 200}, 'foo');       
+//   //   // console.log(err);
+//   //   // console.log(result);
+//   //   assert.end()
+//   // });
+// });
 
-  loader.download(url, function(err, result) {
-    // console.log(err);
-    // console.log(result);
-    assert.end()
-  });
-});
 
->>>>>>> os_check
 // test('MASON_BUCKET not set', function(assert) {
 //   var masonPath = './test/fixtures/mason-versions.ini';
 //   var msg = 'You must set MASON_BUCKET as an environment variable.';
