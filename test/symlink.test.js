974e9e95f989454838cf968e292e4df960c3ba76
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-02-22 07:38:25 -0800

    refactor symlinking

--- test/symlink.test.js
+++ test/symlink.test.js
@@ -2,13 +2,8 @@ var test = require('tape');
 var link = require('../lib/symlink.js');
 var fs = require('fs');
 var path = require('path');
-var sinon = require('sinon');
 var request = require('request');
-var retriever = require('../lib/retrieve_package');
-var rimraf = require('rimraf'); 
-var stream = require('stream');
-var log = require('npmlog');
-var tar = require('tar');
+var rimraf = require('rimraf');
 var fse = require('fs-extra');
 var appDir = process.cwd();
 
@@ -22,27 +17,36 @@ test('setup', (assert) => {
 test('[symlink] builds correct symlink paths', function(assert) {
   var symlinkPath = path.join(global.appRoot, 'test/fixtures/out/mason_packages/.link');
 
-  var libraries = [ { name: 'protozero',
-    version: '1.5.1',
-    headers: true,
-    os: null,
-    awsPath: 'headers/protozero/1.5.1.tar.gz',
-    src: 'https://s3.amazonaws.com/mason-binaries/headers/protozero/1.5.1.tar.gz',
-    dst: appDir + '/test/fixtures/headers/protozero/1.5.1' },
-  { name: 'cairo',
-    version: '1.14.8',
-    headers: null,
-    os: 'osx-x86_64',
-    awsPath: 'osx-x86_64/cairo/1.14.8.tar.gz',
-    src: 'https://s3.amazonaws.com/mason-binaries/osx-x86_64/cairo/1.14.8.tar.gz',
-    dst: appDir + '/test/fixtures/osx-x86_64/cairo/1.14.8' } ]
+  var libraries = [{
+      name: 'protozero',
+      version: '1.5.1',
+      headers: true,
+      os: null,
+      awsPath: 'headers/protozero/1.5.1.tar.gz',
+      src: 'https://s3.amazonaws.com/mason-binaries/headers/protozero/1.5.1.tar.gz',
+      dst: appDir + '/test/fixtures/headers/protozero/1.5.1'
+    },
+    {
+      name: 'cairo',
+      version: '1.14.8',
+      headers: null,
+      os: 'osx-x86_64',
+      awsPath: 'osx-x86_64/cairo/1.14.8.tar.gz',
+      src: 'https://s3.amazonaws.com/mason-binaries/osx-x86_64/cairo/1.14.8.tar.gz',
+      dst: appDir + '/test/fixtures/osx-x86_64/cairo/1.14.8'
+    }
+  ]
 
-  var expected = [ [ appDir + '/test/fixtures/headers/protozero/1.5.1', 
-                    symlinkPath ], 
-                    [ appDir + '/test/fixtures/osx-x86_64/cairo/1.14.8', 
-                    symlinkPath ] ];
+  var expected = [
+    [appDir + '/test/fixtures/headers/protozero/1.5.1',
+      symlinkPath
+    ],
+    [appDir + '/test/fixtures/osx-x86_64/cairo/1.14.8',
+      symlinkPath
+    ]
+  ];
 
-  var result = link.buildLinkPaths(libraries,symlinkPath); 
+  var result = link.buildLinkPaths(libraries, symlinkPath);
   assert.deepEqual(result, expected);
   assert.end();
 });
@@ -50,15 +54,19 @@ test('[symlink] builds correct symlink paths', function(assert) {
 test('[symlink] creates symlink', function(assert) {
   var symlinkPath = path.join(global.appRoot, 'test/fixtures/out/mason_packages/.link');
 
-  var paths = [ [ appDir + '/test/fixtures/headers/protozero/1.5.1', 
-                    symlinkPath ], 
-                    [ appDir + '/test/fixtures/osx-x86_64/cairo/1.14.8', 
-                    symlinkPath ] ];
+  var paths = [
+    [appDir + '/test/fixtures/headers/protozero/1.5.1',
+      symlinkPath
+    ],
+    [appDir + '/test/fixtures/osx-x86_64/cairo/1.14.8',
+      symlinkPath
+    ]
+  ];
 
   var proto = path.join(__dirname + '/fixtures/out', 'mason_packages/.link', 'include', 'protozero', 'byteswap.hpp');
   var cairo = path.join(__dirname + '/fixtures/out', 'mason_packages/.link', 'include', 'cairo', 'cairo-ft.h');
 
-  link.symLink(paths, function(err, result){
+  link.symLink(paths, function(err, result) {
     assert.equal(result, true);
     assert.equal(fs.existsSync(proto), true);
     assert.equal(fs.existsSync(cairo), true);
@@ -69,12 +77,16 @@ test('[symlink] creates symlink', function(assert) {
 test('[symlink] fails to create symlink - directory not found', function(assert) {
   var symlinkPath = path.join(global.appRoot, 'test/fixtures/out/mason_packages/.link');
 
-  var paths = [ [ appDir + '/test/fixtures/headers/protozro/1.5.1', 
-                    symlinkPath ], 
-                    [ appDir + '/test/fixtures/osx-x8_64/cairo/1.14.8', 
-                    symlinkPath ] ];
+  var paths = [
+    [appDir + '/test/fixtures/headers/protozro/1.5.1',
+      symlinkPath
+    ],
+    [appDir + '/test/fixtures/osx-x8_64/cairo/1.14.8',
+      symlinkPath
+    ]
+  ];
 
-  link.symLink(paths, function(err, result){
+  link.symLink(paths, function(err, result) {
     assert.equal(/ENOENT: no such file or directory/.test(err.message), true);
     assert.end();
   });
