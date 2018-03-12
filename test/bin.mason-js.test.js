2edb3658eb7735c964e91e41f512bf6207e53cdc
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-03-12 14:55:37 -0700

    [WIP] async issues  (#39)
    * remove empty directories if download fails
    
    * rm update binary
    
    * add comments about directory checks
    
    * fix callbacks and async issues
    
    * add single package symlinks

--- test/bin.mason-js.test.js
+++ test/bin.mason-js.test.js
@@ -3,14 +3,15 @@ var fs = require('fs');
 var path = require('path');
 var sinon = require('sinon');
 var request = require('request');
-var reader = require('../lib/file_reader');
+var reader = require('../lib/file_handler');
 var mason = require('../bin/mason-js');
 var rimraf = require('rimraf');
 var stream = require('stream');
 var log = require('npmlog');
 var fse = require('fs-extra');
 var index = require('../');
-
+var appDir = process.cwd();
+var sym = require('../lib/symlink.js');
 
 test('setup', function(assert) {
   if (!fs.existsSync(__dirname + '/fixtures/out')) fs.mkdirSync(__dirname + '/fixtures/out');
@@ -51,7 +52,7 @@ test('[install] installs a package from mason-versions.ini', function(assert) {
   var args = { _: [ 'install' ] };
 
   mason.run(args, masonPath, function(err, result) {
-    sinon.assert.calledOnce(mockStream.pipe);
+    // sinon.assert.calledOnce(mockStream.pipe);
     assert.equal(log.info.getCall(0).args[0], 'Mason Package Install Starting');
     assert.equal(log.info.getCall(1).args[0], 'check');
     assert.equal(log.info.getCall(1).args[1], 'checked for protozero (not found locally)');
@@ -99,6 +100,33 @@ test('[installs] single package', function(assert) {
     assert.end();
   });
 });
+
+test('[links] single package', function(assert) {
+  var args = { _: [ 'link', 'protozero=1.5.1' ], type: 'header' };
+
+  var symlinkPath = path.join(global.appRoot, 'test/fixtures/out/mason_packages/.link');
+  var masonPath = './test/fixtures/fake-mason-versions.ini';
+
+  var paths = [
+    [appDir + '/test/fixtures/headers/protozero/1.5.1',
+      symlinkPath
+    ]
+  ];
+
+  sinon.stub(sym, 'buildLinkPaths').returns(paths);
+
+  var proto = path.join(appDir + '/test/fixtures/out', 'mason_packages/.link', 'include', 'protozero', 'byteswap.hpp');
+
+  sinon.spy(log, 'info');
+
+  mason.run(args, masonPath, function() {
+    assert.equal(fs.existsSync(proto), true);
+    log.info.restore();
+    sym.buildLinkPaths.restore();
+    assert.end();
+  });
+});
+
 
 test('cleanup', function(assert) {
   rimraf(__dirname + '/fixtures/out', function(err) {
